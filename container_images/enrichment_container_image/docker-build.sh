343aec9a1fe819409920f8b7d37c8baa1ec8fe76
test63620666-svg <test63620666@gmail.com>
2025-12-05 12:44:14 +0530

    Update docker-build.sh

--- container_images/enrichment_container_image/docker-build.sh
+++ container_images/enrichment_container_image/docker-build.sh
@@ -4,15 +4,17 @@
 #!/bin/bash
 set -eo pipefail
 
+export DOCKER_API_VERSION=1.43
+
 # Build the Docker image with the correct context
 echo "Building Docker image: enrichmentapp"
 echo -e "\n"
-sudo docker build -f ./container_images/enrichment_container_image/Dockerfile -t enrichmentapp . --build-arg BUILDKIT_INLINE_CACHE=1
+sudo DOCKER_API_VERSION=1.43 docker build -f ./container_images/enrichment_container_image/Dockerfile -t enrichmentapp . --build-arg BUILDKIT_INLINE_CACHE=1
 
 # Generate a unique tag for the image
 tag=$(date -u +"%Y%m%d-%H%M%S")
 echo "Tagging image with: $tag"
-sudo docker tag enrichmentapp enrichmentapp:$tag
+sudo DOCKER_API_VERSION=1.43 docker tag enrichmentapp enrichmentapp:$tag
 
 # Output the tag to a file to be used in deployment
 echo -n "$tag" > ./container_images/enrichment_container_image/image_tag.txt
